1ad4b017ffa865331fdcac7dc6264504c5a4e0fb
JunHyeongKim73 <rla5764v@gmail.com>
2023-03-28 23:36:11 +0900

    feat: `Application` 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import baseball.domain.Computer;
+import baseball.service.GameService;
+import baseball.service.GameServiceImpl;
+import baseball.service.NumberGenerator;
+import baseball.service.RandomNumberGenerator;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final NumberGenerator numberGenerator = new RandomNumberGenerator();
+        final Computer computer = new Computer(numberGenerator);
+        final GameService gameService = new GameServiceImpl(computer);
+
+        final BaseBallApplication baseBallApplication = new BaseBallApplication(gameService);
+        baseBallApplication.run();
     }
 }
